dd3418bb09c31ed90da8f913e87aa98bf7b1079a
Gamepro5 <gamepro543@gmail.com>
2021-04-16 11:59:17 -0700

    CSS warm up. Brain booting up.

---
+++ const.js
@@ -0,0 +1,22 @@
+if (document.querySelector(".navigation") !== null) {
+document.querySelector(".navigation").innerHTML = `
+
+<div class="nav">
+
+<a href="/art" class="nav_button"><img class="nav_button_img" src="/images/nav/art.png"></a>
+<a href="/art" class="nav_button"><img class="nav_button_img" src="/images/nav/art.png"></a>
+<a href="/art" class="nav_button"><img class="nav_button_img" src="/images/nav/art.png"></a>
+<a href="/art" class="nav_button"><img class="nav_button_img" src="/images/nav/art.png"></a>
+<a href="/art" class="nav_button"><img class="nav_button_img" src="/images/nav/art.png"></a>
+
+</div>
+
+
+`
+document.querySelector(".header").innerHTML = `
+
+<div style="background-color: orange;">MUSHEMIIA pog</div>
+
+
+`
+}
